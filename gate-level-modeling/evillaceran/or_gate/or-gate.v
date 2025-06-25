b9bde4d5d1a8b0f89707d9940d49ac129534cdd7
evillaceran <villaceraneric3@gmail.com>
2025-06-25 14:33:29 +0800

    laboratory exercise 001 by evillaceran

---
+++ gate-level-modeling/evillaceran/or_gate/or-gate.v
@@ -0,0 +1,18 @@
+//-----------------------------------------------------
+// Laboratory Experiment 001
+// Design Name : or_gate
+// File Name : or_gate.v
+// Function : Implement OR logic gate
+// Designer: eric villaceran
+// Period: Term 3 AY24-25
+//-----------------------------------------------------
+
+module or_gate(
+  input A,B,
+  output C
+  );
+  
+  // Gate type
+  or  evillaceran (C, A, B);
+
+endmodule 
---
+++ gate-level-modeling/evillaceran/xor_gate/xor_gate.v
@@ -0,0 +1,18 @@
+//-----------------------------------------------------
+// Laboratory Experiment 001
+// Design Name : xor_gate
+// File Name : xor_gate.v
+// Function : Implement AND logic gate
+// Designer: eric villaceran
+// Period: Term 3 AY24-25
+//-----------------------------------------------------
+
+module xor_gate(
+  input A,B,
+  output C
+  );
+  
+  // Gate type
+  xor  evillaceran (C, A, B);
+
+endmodule
