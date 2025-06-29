dc59750c1328b37660012be3aa73ee7906a83188
Ernesto583 <ernestomartinez09356@gmail.com>
2025-06-29 14:00:36 +0800

    laboratory excercise 1 intro hdl

--- gate-level-modeling/emartinez/and_gate/and_gate.v
+++ gate-level-modeling/emartinez/and_gate/and_gate.v
@@ -1,18 +1,18 @@
-    //-----------------------------------------------------
+//-----------------------------------------------------
 // Laboratory Experiment 001
-// Design Name : nor_gate
-// File Name : nor_gate.v
+// Design Name : and_gate
+// File Name : and_gate.v
 // Function : Implement AND logic gate
-// Designer: eric villaceran
+// Designer: Ernesto Martinez
 // Period: Term 3 AY24-25
 //-----------------------------------------------------
 
-module nor_gate(
+module and_gate(
   input A,B,
   output C
   );
   
   // Gate type
-  nor  evillaceran (C, A, B);
+  and  emartinez (C, A, B);
 
 endmodule
