dba714a270935b5dbdb10777c372709d03bb12a6
jiv04 <jayviesevero04@gmail.com>
2025-07-01 01:14:30 +0800

    laboratory excercise 1 intro hdl - jayvie

---
+++ gate-level-modeling1/jsevero/not_gate/not_gate/not_gate.v
@@ -0,0 +1,15 @@
+//-----------------------------------------------------
+// Laboratory Experiment 001
+// Design Name  : not_gate
+// File Name    : not_gate.v
+// Function     : Implement NOT logic gate (inverter)
+// Designer     : jayvie severo
+// Period       : Term 3 AY24-25
+//-----------------------------------------------------
+module not_gate(
+  input  A,
+  output C
+);
+  // Gate type
+  not jsevero (C, A);
+endmodule
