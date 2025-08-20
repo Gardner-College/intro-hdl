b6a58d7ad91c145bb5a0de58942b66f514ad4339
Paclaa <paclarunel022@gmail.com>
2025-08-20 23:11:59 +0800

    machine problem hdl-gate level

---
+++ gate-level-modelling/ajesalva/nor_gate/nor_gate.v
@@ -0,0 +1,16 @@
+//-----------------------------------------------------
+// Laboratory Experiment 001
+// Design Name  : nor_gate
+// File Name    : nor_gate.v
+// Function     : Implement NOR logic gate
+// Designer     : jesalva andrei
+// Period       : Term 3 AY24-25
+//-----------------------------------------------------
+module nor_gate(
+  input  A,
+  input  B,
+  output C
+);
+  // Gate type
+  nor jandrei (C, A, B);
+endmodule
