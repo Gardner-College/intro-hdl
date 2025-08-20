cb86767350888bc5c14ece20d7a6d3a28c4ca9d6
Paclaa <paclarunel022@gmail.com>
2025-08-20 23:11:59 +0800

    machine problem hdl-gate level

---
+++ gate-level-modelling/ajesalva/xnor_gate/xnor_gate.v
@@ -0,0 +1,16 @@
+//-----------------------------------------------------
+// Laboratory Experiment 001
+// Design Name  : xnor_gate
+// File Name    : xnor_gate.v
+// Function     : Implement XNOR logic gate
+// Designer     : jesalva andrei
+// Period       : Term 3 AY24-25
+//-----------------------------------------------------
+module xnor_gate(
+  input  A,
+  input  B,
+  output C
+);
+  // Gate type
+  xnor jandrei (C, A, B);
+endmodule
